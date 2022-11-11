b49dc32f442c2180a684af51cb1ca5c1878f8a03
Ko-Eunseo <koyk0408@naver.com>
2022-11-11 17:13:26 +0900

    fix : eslint error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,11 +3,12 @@ export default {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: "babel-eslint"
   },
   plugins: ['react'],
   rules: {},
