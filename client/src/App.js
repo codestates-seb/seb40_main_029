9a960a471d4f1f5f70e5e03420af73e9874fbd47
Ko-Eunseo <koyk0408@naver.com>
2022-11-12 23:53:13 +0900

    header 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 폰트테스트
-        </a>
-      </header>
+      <Header />
     </div>
   );
 };
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,14 +1,26 @@
 import styled from "styled-components";
+import './header.css';
 import Button from "../common/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStar,
+  faMinus
+} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <>
-      <h2>오늘은 어떤가요?</h2>
+      <h1>오늘은
+        <br/>어떤가요?</h1>
         <Button size="long" disabled>
           북마크
         </Button>
-        <Button size="circle">+</Button>
+        <Button className="iconBtn" size="circle">
+          <FontAwesomeIcon icon={faStar} />
+        </Button>
+        <Button className="iconBtn" size="circle">
+          <FontAwesomeIcon icon={faMinus} size="lg" />
+        </Button>
     </>
   );
 }
