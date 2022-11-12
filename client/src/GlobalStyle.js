068538ec86d114c1f9caf8a374ba89b24339a44a
kosmos992 <yechan5870@gmail.com>
2022-11-12 15:22:21 +0900

    fix: eslint 한시적 삭제 및 폰트 적용

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -1,17 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
+import './fonts/pretendard-subset.css';
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
   // You can continue writing global styles here
   * {
-    font-family: -apple-system,BlinkMacSystemFont,monospace,sans-serif;
+    font-family: 'Pretendard';
     box-sizing: border-box;
     text-decoration: none;
     padding: 0;
     margin: 0;
     /* border-radius: ; */
     line-height: 1.5;
+
   }
   body {
     background-color: #F1F2F3;
@@ -19,4 +21,5 @@ export const GlobalStyle = createGlobalStyle`
   a, button {
     cursor: pointer;
   }
+
 `;
