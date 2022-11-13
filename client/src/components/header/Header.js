0e9821c4e9671a902c007925199e95c3d08cdc00
Ko-Eunseo <koyk0408@naver.com>
2022-11-13 12:28:06 +0900

    feat : header 오른쪽영역(유저)

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -4,32 +4,52 @@ import Button from "../common/Button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faStar,
-  faMinus
+  faMinus,
+  faCertificate
 } from '@fortawesome/free-solid-svg-icons';
 
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 16px;
+`;
 const HeaderTitle = styled.h1`
   line-height: 1.2;
   font-size: 48px;
-  margin-bottom: 8px;
+  margin: 0;
   margin-left: 16px;
+`;
+const User = styled.span`
+margin: 0 16px;
+  svg {
+    margin-right: 8px;
+  }
 `;
 
 function Header() {
   return (
-    <>
-      <HeaderTitle>오늘은
-        <br/>어떤가요?
-      </HeaderTitle>
-        <Button size="long" disabled>
-          북마크
-        </Button>
-        <Button size="circle">
-          <FontAwesomeIcon icon={faStar} />
-        </Button>
-        <Button size="circle">
-          <FontAwesomeIcon icon={faMinus} size="lg" />
-        </Button>
-    </>
+    <HeaderWrapper>
+      <div>
+        <HeaderTitle>오늘은
+          <br/>어떤가요?
+        </HeaderTitle>
+          <Button size="long" disabled>
+            북마크
+          </Button>
+          <Button size="circle">
+            <FontAwesomeIcon icon={faStar} />
+          </Button>
+          <Button size="circle">
+            <FontAwesomeIcon icon={faMinus} size="lg" />
+          </Button>
+      </div>
+      <div>
+        <User>
+          <FontAwesomeIcon icon={faCertificate} />
+          USERNAME
+        </User>
+      </div>
+    </HeaderWrapper>
   );
 }
 
