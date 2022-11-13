d52f8614d4fe1faad2ef63467bd8ed88d7922c1b
Mintaek_CHO <mintaek5555@gmail.com>
2022-11-13 16:35:00 +0900

    fix : mapper 수정

--- server/src/main/java/com/codestates/mainproject/member/mapper/MemberMapper.java
+++ server/src/main/java/com/codestates/mainproject/member/mapper/MemberMapper.java
@@ -2,13 +2,16 @@ package com.codestates.mainproject.member.mapper;
 
 
 
+import com.codestates.mainproject.member.dto.MemberPaletteResponseDto;
 import com.codestates.mainproject.member.dto.MemberPatchDto;
 import com.codestates.mainproject.member.dto.MemberPostDto;
 import com.codestates.mainproject.member.dto.MemberResponseDto;
 import com.codestates.mainproject.member.entity.Member;
+import com.codestates.mainproject.palette.entity.MemberPalette;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -20,4 +23,16 @@ public interface MemberMapper {
     MemberResponseDto memberToMemberResponseDto(Member member);
 
     List<MemberResponseDto> membersToMemberResponseDtos(List<Member> members);
+
+    default List<MemberPaletteResponseDto> memberTomemberPaletteResponseDtos(
+            List<MemberPalette> memberPalettes) {
+        return memberPalettes
+                .stream()
+                .map(memberPalette -> MemberPaletteResponseDto
+                        .builder()
+                        .paletteName(memberPalette.getMoodPalette().getPaletteName())
+                        .paletteCode(memberPalette.getMoodPalette().getPaletteCode())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
