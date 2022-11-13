345a95d99e1dc8c442bb7822e6f56f2dcfee2412
Ko-Eunseo <koyk0408@naver.com>
2022-11-13 12:07:23 +0900

    feat : button padding 조정, font-weight 조정

--- client/src/components/common/Button.js
+++ client/src/components/common/Button.js
@@ -2,11 +2,11 @@ import styled, { css } from "styled-components";
 
 const SIZES = {
   circle: css`
-    --button-padding: 8px 10px;
+    --button-padding: 10px;
     --button-radius: 50%;
   `,
   long: css`
-    --button-padding: 8px 16px;
+    --button-padding: 10px 16px;
     --button-radius: 30px;
   `
 };
@@ -32,6 +32,7 @@ const Btn = styled.button`
   box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #FAF8FF;
   border: none;
   margin-right: 10px;
+  font-weight: 700;
 
   &:active,
   &:hover,
