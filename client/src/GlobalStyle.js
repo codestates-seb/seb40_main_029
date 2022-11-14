57e9a6c38371b539bffb295d49261761bf521def
kosmos992 <yechan5870@gmail.com>
2022-11-14 11:45:04 +0900

    fix: eslint 적용

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -13,10 +13,11 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     /* border-radius: ; */
     line-height: 1.5;
+    color: #333435;
 
   }
   body {
-    background-color: #F1F2F3;
+    background-color: #F6F6F6;
   }
   a, button {
     cursor: pointer;
