9cb987b1e4937f5466a547bca4450d041b61a4a5
kosmos992 <yechan5870@gmail.com>
2022-11-14 18:06:53 +0900

    feat: 모달, 카드 컴포넌트 구현'

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
+import { TodoModal, MailModal, StoreModal } from './components/modal/Modal';
+import MoodCard from './components/card/MoodCard';
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <TodoModal />
+      <>--------</>
+      <MailModal />
+      <>--------</>
+      <StoreModal />
+      <>--------</>
+      <MoodCard />
+      <>--------</>
+    </>
+  );
 };
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React 폰트테스트
-//         </a>
-//       </header>
-//     </div>
-//   );
-// };
 
 export default App;
