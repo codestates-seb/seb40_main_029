610481af529578e7c5c4c89d0a701c45bc949d59
Ko-Eunseo <koyk0408@naver.com>
2022-11-14 12:31:34 +0900

    feat : nav 디자인 최종 결정

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -10,52 +10,6 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const Bubble = styled.nav`
-  list-style: none;
-  background-color: #f6f6f6;
-  padding: 16px 8px;
-  margin: 8px;
-  margin-top: 24px;
-  border-radius: 20px;
-  position: relative;
-  filter: drop-shadow(3px 3px 5px rgba(0, 0, 0, 0.25));
-
-  &:after {
-    content: '';
-    position: absolute;
-    top: -25px;
-    left: 10%;
-    width: 2px;
-    background-color: transparent;
-    border-bottom: 30px solid #f6f6f6;
-    border-left: 15px solid transparent;
-    border-right: 15px solid transparent;
-    border-top: 10px solid transparent;
-  }
-
-  svg {
-    width: 30px;
-    padding-right: 10px;
-    path {
-      color: #656565;
-    }
-  }
-
-  li {
-    list-style: none;
-    border-radius: 20px;
-    padding: 4px 8px;
-    margin: 2px 0;
-    span {
-      font-size: 14px;
-    }
-    &:hover,
-    &:active {
-      background-color: #fff;
-    }
-  }
-`;
-
-const Bubble2 = styled.nav`
   background-color: #f6f6f6;
   padding: 16px 0;
   margin: 8px;
@@ -130,35 +84,6 @@ const Nav = () => {
           </li>
         </ul>
       </Bubble>
-
-      {/* <Bubble2>
-      <ul>
-          <li>
-            <FontAwesomeIcon icon={faEnvelope} size='lg' />
-            <span>편지함</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faHighlighter} size='lg' />
-            <span>오늘할일</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faStore} size='lg' />
-            <span>색상테마</span>
-          </li>
-          <li>
-              <FontAwesomeIcon icon={faUserGroup} size='lg' />
-              <span>친구</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faCalendarDays} size='lg' />
-            <span>기록</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faRightFromBracket} size='lg' />
-            <span>로그아웃</span>
-          </li>
-        </ul>
-      </Bubble2> */}
     </>
   );
 };
