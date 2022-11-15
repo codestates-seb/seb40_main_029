5cbe0a3963657f4b6197cb89bc512ae5a82bc81e
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 15:40:28 +0900

    feat : input 스타일

--- client/src/components/atoms/Input.js
+++ client/src/components/atoms/Input.js
@@ -1,5 +1,14 @@
+import styled, { css } from 'styled-components';
+
+const InputItem = styled.input`
+  border: none;
+  padding: 4px 8px;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
+`;
 const Input = ({ placeHolder, value }) => {
-  return <input {...value} type="text" placeholder={placeHolder} />;
+  return <InputItem {...value} type="text" placeholder={placeHolder} />;
 };
 
 export default Input;
