a8c02f79ea48f929e091be482ff38e1ac9144df2
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 18:19:06 +0900

    feat : letters 더미데이터로 렌더링

---
+++ client/src/components/module/Letters.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import Button from '../atoms/Button';
+import ShadowBox from '../atoms/ShadowBox';
+import LetterItem from './LetterItem';
+import { MailModal } from './modal/Modal';
+
+const ButtonLayout = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin: 8px;
+`;
+
+const Letters = () => {
+  const dummyLetter = [
+    {
+      username: '은서',
+      createdAt: 'Tue Nov 15 2022 17:52:09 GMT+0900',
+      body: '뭐하세요?',
+    },
+    {
+      username: '예륜',
+      createdAt: 'Tue Nov 15 2022 17:52:09 GMT+0900',
+      body: '낙낙INVU우우우우우우우우INVU열이올라낙낙INVU우우우우우우우우INVU열이올라요노동요낙낙INVU우우우우우우우우INVU열이올라요노동요낙낙INVU우우우우우우우우INVU열이올라요노동요낙낙INVU우우우우우우우우INVU열이올라요노동요요노동요낙낙INVU우우우우우우우우INVU열이올라요노동요낙낙INVU우우우우우우우우INVU열이올라요노동요',
+    },
+  ];
+  return (
+    <>
+      <MailModal>
+        {dummyLetter ? (
+          dummyLetter.map((letter, i) => {
+            return <LetterItem key={i} data={letter} />;
+          })
+        ) : (
+          <ShadowBox>받은 편지가 없습니다.</ShadowBox>
+        )}
+        <ButtonLayout>
+          <Button size="circle">+</Button>
+        </ButtonLayout>
+      </MailModal>
+    </>
+  );
+};
+
+export default Letters;
