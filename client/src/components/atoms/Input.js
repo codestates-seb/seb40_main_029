5ba7ec5c8e84fe2090854d6a79ecbce7e119506b
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 20:01:34 +0900

    feat : input size props로 넘기도록 함

--- client/src/components/atoms/Input.js
+++ client/src/components/atoms/Input.js
@@ -1,14 +1,32 @@
 import styled, { css } from 'styled-components';
 
+const SIZES = {
+  long: css`
+    --input-height: 200px;
+  `,
+};
+
 const InputItem = styled.input`
+  ${props => props.sizeStyle}
+  height: var(--input-height);
   border: none;
   padding: 4px 8px;
   border-radius: 10px;
   margin-bottom: 16px;
   box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
+  margin: 8px 0;
 `;
-const Input = ({ placeHolder, value }) => {
-  return <InputItem {...value} type="text" placeholder={placeHolder} />;
+
+const Input = ({ placeHolder, value, size }) => {
+  const sizeStyle = SIZES[size];
+  return (
+    <InputItem
+      sizeStyle={sizeStyle}
+      {...value}
+      type="text"
+      placeholder={placeHolder}
+    />
+  );
 };
 
 export default Input;
