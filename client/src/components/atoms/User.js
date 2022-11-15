561763fb316c1b8a2ea61591684e70dac7837a0e
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 18:18:02 +0900

    feat : username props로 받도록 처리

--- client/src/components/atoms/User.js
+++ client/src/components/atoms/User.js
@@ -3,18 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCertificate } from '@fortawesome/free-solid-svg-icons';
 
 const Username = styled.span`
-  margin: 0 30px; /* nav가 오픈되었을때 여부와 상관없이 위치고정 */
+  margin-right: 30px; /* nav가 오픈되었을때 여부와 상관없이 위치고정 */
   font-size: 14px;
   svg {
     margin-right: 8px;
   }
 `;
 
-const User = ({ onClick }) => {
+const User = ({ onClick, children }) => {
   return (
     <Username onClick={onClick}>
       <FontAwesomeIcon icon={faCertificate} />
-      USERNAME
+      {children}
     </Username>
   );
 };
