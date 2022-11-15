0104d744234fc956bdf985e624478ec188324b69
YeryunJung <2bites@naver.com>
2022-11-15 15:15:53 +0900

    #24 - feat: 로그인, 로그아웃 틀 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import Header from './components/module/Header';
+import Login from './components/templates/Login';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      <Login />
     </div>
   );
 };
