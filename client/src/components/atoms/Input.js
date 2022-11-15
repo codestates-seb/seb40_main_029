517b67f4d38be6853b205864a4c513410312933c
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 21:27:01 +0900

    feat: input style props 추가

--- client/src/components/atoms/Input.js
+++ client/src/components/atoms/Input.js
@@ -6,22 +6,34 @@ const SIZES = {
   `,
 };
 
+const BORDER = {
+  shadow: css`
+    --input-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
+  `,
+  transparent: css`
+    --input-shadow: none;
+  `,
+};
+
 const InputItem = styled.input`
-  ${props => props.sizeStyle}
+  ${props => props.sizeStyle};
+  ${props => props.borderStyle};
   height: var(--input-height);
   border: none;
   padding: 4px 8px;
   border-radius: 10px;
   margin-bottom: 16px;
-  box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
+  box-shadow: var(--input-shadow);
   margin: 8px 0;
 `;
 
-const Input = ({ placeHolder, value, size }) => {
+const Input = ({ placeHolder, value, size, border }) => {
   const sizeStyle = SIZES[size];
+  const borderStyle = BORDER[border];
   return (
     <InputItem
       sizeStyle={sizeStyle}
+      borderStyle={borderStyle}
       {...value}
       type="text"
       placeholder={placeHolder}
