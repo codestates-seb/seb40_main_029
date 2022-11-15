61bb41fdd1b274e19c3bdaf40d19882d710db208
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 18:17:34 +0900

    feat : shadowBox atom 컴포넌트

---
+++ client/src/components/atoms/ShadowBox.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  margin: 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
+  min-width: 600px;
+  padding: 16px 30px;
+`;
+
+const ShadowBox = ({ children, onClick }) => {
+  return (
+    <>
+      <Box onClick={onClick}>{children}</Box>
+    </>
+  );
+};
+
+export default ShadowBox;
