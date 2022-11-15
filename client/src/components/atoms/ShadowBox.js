ad5330a52e7ffd5472657833d66ba81f6eaabb55
Ko-Eunseo <koyk0408@naver.com>
2022-11-15 21:26:32 +0900

    feat : shadowbox size props 추가

--- client/src/components/atoms/ShadowBox.js
+++ client/src/components/atoms/ShadowBox.js
@@ -1,16 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+const SIZES = {
+  long: css`
+    --shadowBox-height: 100%;
+  `,
+};
 const Box = styled.div`
+  ${props => props.sizeStyle};
   margin: 16px;
   border-radius: 10px;
   box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
   padding: 16px 30px;
+  height: var(--shadowBox-height);
 `;
 
-const ShadowBox = ({ children, onClick }) => {
+const ShadowBox = ({ children, onClick, size }) => {
+  const sizeStyle = SIZES[size];
   return (
     <>
-      <Box onClick={onClick}>{children}</Box>
+      <Box onClick={onClick} sizeStyle={sizeStyle}>
+        {children}
+      </Box>
     </>
   );
 };
