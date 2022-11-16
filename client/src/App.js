2f2f8072cd12f0053a4c62ef880e496a04450971
Ko-Eunseo <koyk0408@naver.com>
2022-11-16 20:29:23 +0900

    feat : nav 편지함 링크 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import Header from './components/module/Header';
+import MoodCard from './components/module/MoodCard';
 import Letter from './components/templates/Letter';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Letter />
+
+      <Routes>
+        <Route path="/letter" element={<Letter />} />
+      </Routes>
     </div>
   );
 };
