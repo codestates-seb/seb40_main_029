c27e45aa8a96d7213f9f9a62432d5c4c3d83f1b4
Ko-Eunseo <koyk0408@naver.com>
2022-11-17 20:13:27 +0900

    feat : bookmark 기능

--- client/src/components/module/Bookmark.js
+++ client/src/components/module/Bookmark.js
@@ -2,29 +2,49 @@ import styled from 'styled-components';
 import Button from '../atoms/Button';
 import { useEffect, useState } from 'react';
 import BookmarkHandle from './BookmarkHandle';
+import BookmarkDelete from '../module/BookmarkDelete';
+const BookItem = styled.div`
+  position: relative;
 
+  &:hover > span {
+    display: inline-block;
+  }
+`;
 const Anchor = styled.a`
   line-height: 1;
 `;
-
 const Bookmark = () => {
-  const [booksArr, setBookmarkArr] = useState(
-    JSON.parse(localStorage.getItem('bookmark') || 0)
-  );
-  const [isOpen, setIsOpen] = useState(false);
-  let renderBookmark = [];
+  const dummyBookmark = [
+    { name: 'naver', url: 'https://www.naver.com' },
+    { name: 'google', url: 'https://www.google.com' },
+  ];
+  const [booksArr, setBookmarkArr] = useState(dummyBookmark);
+  // ------------- 여기서 에러남 ----------------
+  //   const [booksArr, setBookmarkArr] = useState(
+  //   JSON.parse(localStorage.getItem('bookmark') || dummyBookmark)
+  // );
+  // --------------왜일까 ----------------------
 
-  useEffect(() => {
-    renderBookmark = JSON.parse(localStorage.getItem('bookmark'));
-  }, []);
+  const renderBookmark = JSON.parse(localStorage.getItem('bookmark'));
+  const bookmarkDelete = e => {
+    // alert('북마크를 삭제하시겠습니까?');
+    booksArr.filter(book => {
+      return e.target.value !== book;
+    });
+    console.log(e.target.value);
+    // localStorage.removeItem();
+  };
   return (
     <>
       {booksArr.length
         ? booksArr.map((book, i) => {
             return (
-              <Button key={i} size="long" fontsize="middle">
-                <Anchor href={book.url}>{book.name}</Anchor>
-              </Button>
+              <BookItem key={i}>
+                <Button size="long" fontsize="middle">
+                  <Anchor href={book.url}>{book.name}</Anchor>
+                </Button>
+                <BookmarkDelete onClick={bookmarkDelete} />
+              </BookItem>
             );
           })
         : null}
--- client/src/components/module/BookmarkHandle.js
+++ client/src/components/module/BookmarkHandle.js
@@ -1,10 +1,73 @@
+// import styled from 'styled-components';
+// import Button from '../atoms/Button';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faStar, faMinus } from '@fortawesome/free-solid-svg-icons';
+// import { useState } from 'react';
+// import BookmarkCreate from './BookmarkCreate';
+// import BookmarkEdit from './BookmarkEdit';
+
+// const LightIcon = styled.span`
+//   svg {
+//     vertical-align: 0 !important; /* fontawsome icon 가운데정렬 */
+//   }
+//   path {
+//     color: #cbcccd;
+//   }
+// `;
+// export const CenterLayout = styled.div`
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// `;
+
+// const BookmarkHandle = ({ booksArr, setBookmarkArr }) => {
+//   const [addBtnIsOpen, setAddBtnIsOpen] = useState(false);
+//   const [ListIsOpen, setListIsOpen] = useState(false);
+//   const onClickAddBtn = () => {
+//     setAddBtnIsOpen(!addBtnIsOpen);
+//   };
+//   const handleBookmarkEdit = () => {
+//     setListIsOpen(!ListIsOpen);
+//   };
+//   return (
+//     <>
+//       <Button size="circle" onClick={onClickAddBtn}>
+//         <LightIcon>
+//           <FontAwesomeIcon icon={faStar} />
+//         </LightIcon>
+//       </Button>
+//       <Button size="circle" onClick={handleBookmarkEdit}>
+//         <LightIcon>
+//           <FontAwesomeIcon icon={faMinus} size="lg" />
+//         </LightIcon>
+//       </Button>
+//       {addBtnIsOpen ? (
+//         <CenterLayout>
+//           <BookmarkCreate
+//             setAddBtnIsOpen={setAddBtnIsOpen}
+//             setBookmarkArr={setBookmarkArr}
+//             booksArr={booksArr}
+//           />
+//         </CenterLayout>
+//       ) : null}
+//       {ListIsOpen ? (
+//         <CenterLayout>
+//           <BookmarkEdit />
+//         </CenterLayout>
+//       ) : null}
+//     </>
+//   );
+// };
+
+// export default BookmarkHandle;
+
 import styled from 'styled-components';
 import Button from '../atoms/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import BookmarkCreate from './BookmarkCreate';
-import BookmarkEdit from './BookmarkEdit';
 
 const LightIcon = styled.span`
   svg {
@@ -23,23 +86,14 @@ export const CenterLayout = styled.div`
 
 const BookmarkHandle = ({ booksArr, setBookmarkArr }) => {
   const [addBtnIsOpen, setAddBtnIsOpen] = useState(false);
-  const [ListIsOpen, setListIsOpen] = useState(false);
   const onClickAddBtn = () => {
     setAddBtnIsOpen(!addBtnIsOpen);
-  };
-  const handleBookmarkEdit = () => {
-    setListIsOpen(!ListIsOpen);
   };
   return (
     <>
       <Button size="circle" onClick={onClickAddBtn}>
         <LightIcon>
           <FontAwesomeIcon icon={faStar} />
-        </LightIcon>
-      </Button>
-      <Button size="circle" onClick={handleBookmarkEdit}>
-        <LightIcon>
-          <FontAwesomeIcon icon={faMinus} size="lg" />
         </LightIcon>
       </Button>
       {addBtnIsOpen ? (
@@ -49,11 +103,6 @@ const BookmarkHandle = ({ booksArr, setBookmarkArr }) => {
             setBookmarkArr={setBookmarkArr}
             booksArr={booksArr}
           />
-        </CenterLayout>
-      ) : null}
-      {ListIsOpen ? (
-        <CenterLayout>
-          <BookmarkEdit />
         </CenterLayout>
       ) : null}
     </>
