b62550c7c2185cf24d7c4dd631ffcb85da5812b1
YeryunJung <2bites@naver.com>
2022-11-22 22:31:08 +0900

    #39 - feat: env 추가

--- client/src/api/PaletteShop.js
+++ client/src/api/PaletteShop.js
@@ -5,11 +5,11 @@ import { memberIdSelector, paletteCodeSelector } from '../redux/hooks';
 axios.defaults.withCredentials = true;
 
 export const PaletteList = async () => {
-  // const API_URL = process.env.REACT_APP_SERVER_API_URL;
+  const API_URL = process.env.REACT_APP_SERVER_API_URL;
   const path = '/palette';
   try {
     // 팔레트가 담긴 배열
-    await axios.get('/api' + path).then(res => console.log(res));
+    await axios.get(API_URL + path).then(res => console.log(res.data));
     // const paletteSet = [];
     // for (let i = 0; i < result.length; i += 8)
     //   paletteSet.push(result.slice(i, i + 8));
