a8f7785b2824b962d7368105fcaae8fe9de9557a
Ko-Eunseo <koyk0408@naver.com>
2022-11-23 19:41:26 +0900

    feat : 필요없는 코드 삭제

--- client/src/components/templates/Letter.js
+++ client/src/components/templates/Letter.js
@@ -5,19 +5,11 @@ import { useState } from 'react';
 import LetterCreate from '../module/LetterCreate';
 import { ContentLayout } from '../atoms/Layouts';
 
-// export const ContentLayout = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
 const Letter = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <ContentLayout>
-      <MoodCard />
       {isOpen ? (
         <LetterCreate />
       ) : (
