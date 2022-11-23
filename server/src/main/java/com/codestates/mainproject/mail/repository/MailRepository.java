9b7c24b695a9325ced0fc4cac746a5ee40d16a9d
Mintaek_CHO <mintaek5555@gmail.com>
2022-11-23 16:12:48 +0900

    fix : 어제 보낸 메일 오늘 확인할 수 있도록 변경

--- server/src/main/java/com/codestates/mainproject/mail/repository/MailRepository.java
+++ server/src/main/java/com/codestates/mainproject/mail/repository/MailRepository.java
@@ -4,11 +4,14 @@ import com.codestates.mainproject.mail.entity.Mail;
 import com.codestates.mainproject.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MailRepository extends JpaRepository <Mail, Long> {
 
     List<Mail> findAllByReceiverMemberId(Long memberId);
+
+    List<Mail> findAllByReceiverMemberIdAndAndCreatedAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
 
     void deleteAllByReceiver_MemberId(Long memberId);
 }
