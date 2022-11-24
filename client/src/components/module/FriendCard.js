9ceb922034131bf13161c78e45809d8846906909
Ko-Eunseo <koyk0408@naver.com>
2022-11-24 13:29:35 +0900

    feat : 미니무드카드 컴포넌트 분리

--- client/src/components/module/FriendCard.js
+++ client/src/components/module/FriendCard.js
@@ -1,35 +1,21 @@
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { deleteFriend, getSpecificPalette } from '../../api/FriendDataApi';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
+import MiniCard from '../atoms/MiniCard';
 
-const Card = styled.div`
-  width: 100px;
-  height: 120px;
-  box-shadow: 2px 2px 5px rgba(22, 27, 29, 0.25), -2px -2px 5px #faf8ff;
-  margin: 16px;
+const FriendCardWrap = styled.div`
   position: relative;
   &:hover > span {
     display: inline-block;
   }
 `;
-const MoodPic = styled.div`
-  width: 100px;
-  height: 90px;
-  border: 4px solid white;
-  background-color: ${props => (props.color ? `#${props.color}` : '#faf8ff')};
-`;
-const FriendName = styled.span`
-  margin-left: 4px;
-  font-size: 13px;
-`;
 const EditBtn = styled.span`
   position: absolute;
-  right: 0;
+  right: 16px;
+  bottom: 24px;
   display: none;
-
   &:hover {
     opacity: 0.7;
   }
@@ -48,7 +34,9 @@ const FriendCard = ({ friend }) => {
   }, []);
 
   const friendsColor = palette?.find(mycolor => {
-    return mycolor.mood === friend.respondentMoodPaletteDetails.mood;
+    if (friend.respondentMoodPaletteDetails) {
+      return mycolor.mood === friend.respondentMoodPaletteDetails.mood;
+    }
   });
 
   const handleDeleteFriend = () => {
@@ -61,14 +49,18 @@ const FriendCard = ({ friend }) => {
   };
 
   return (
-    <Card>
-      <MoodPic color={friendsColor?.colorCode}></MoodPic>
-      <FriendName>{friend.respondentDisplayName}</FriendName>
-      <FriendName>{friend.respondentMoodPaletteDetails.mood}</FriendName>
+    <FriendCardWrap>
+      <MiniCard
+        color={friendsColor?.colorCode}
+        contents={friend.respondentDisplayName}
+        mood={friend.respondentMoodPaletteDetails?.mood}
+        onClick={handleDeleteFriend}
+        icon={faDeleteLeft}
+      ></MiniCard>
       <EditBtn onClick={handleDeleteFriend}>
         <FontAwesomeIcon icon={faDeleteLeft} />
       </EditBtn>
-    </Card>
+    </FriendCardWrap>
   );
 };
 
