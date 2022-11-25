82748f8d40d3d42b5606df8d40c8bc8582cd4975
Ko-Eunseo <koyk0408@naver.com>
2022-11-25 11:46:17 +0900

    feat : 편지함 스크롤(임시)

--- client/src/components/module/Letters.js
+++ client/src/components/module/Letters.js
@@ -5,7 +5,13 @@ import { MailModal } from './Modal';
 import { useEffect, useState } from 'react';
 import { getAllMails } from '../../api/MailDataApi';
 import { RightBottomLayout } from '../atoms/Layouts';
+import styled from 'styled-components';
 
+const ContentWrap = styled.div`
+  //임시방편. 페이지네이션 구현 예정
+  overflow-y: scroll;
+  height: 396px;
+`;
 const Letters = ({ setIsOpen, isOpen }) => {
   const [mails, setMails] = useState();
   const handleLetterCreate = () => {
@@ -22,13 +28,15 @@ const Letters = ({ setIsOpen, isOpen }) => {
   return (
     <>
       <MailModal>
-        {mails ? (
-          mails.map((mail, i) => {
-            return <LetterItem key={i} data={mail} />;
-          })
-        ) : (
-          <ShadowBox>받은 편지가 없습니다.</ShadowBox>
-        )}
+        <ContentWrap>
+          {mails ? (
+            mails.map((mail, i) => {
+              return <LetterItem key={i} data={mail} />;
+            })
+          ) : (
+            <ShadowBox>받은 편지가 없습니다.</ShadowBox>
+          )}
+        </ContentWrap>
         <RightBottomLayout>
           <Button size="circle" onClick={handleLetterCreate}>
             +
