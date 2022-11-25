d6d9f767be6dc1235e25865445ac1643db680d0a
Ko-Eunseo <koyk0408@naver.com>
2022-11-25 18:51:53 +0900

    feat : home 무드카드 레이아웃

--- client/src/components/templates/Home.js
+++ client/src/components/templates/Home.js
@@ -1,9 +1,14 @@
+import { ContentLayout } from '../atoms/Layouts';
 import Header from '../module/Header';
+import MoodCard from '../module/MoodCard';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <ContentLayout>
+        <MoodCard />
+      </ContentLayout>
     </>
   );
 };
