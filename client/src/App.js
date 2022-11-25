64a22d1d3ab4d2e4874dd3eaaaf5116ce1933e8e
Ko-Eunseo <koyk0408@naver.com>
2022-11-25 13:54:39 +0900

    feat : Home 페이지 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Friends from './components/templates/Friends';
-import Header from './components/module/Header';
-import Letter from './components/templates/Letter';
+import Home from './components/templates/Home';
+// import Login from './templates/Login';
+// import Signup from './templates/Signup';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Routes>
-        <Route path="/letter" element={<Letter />} />
-        <Route path="/friends" element={<Friends />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
       </Routes>
     </div>
   );
