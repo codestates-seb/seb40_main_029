501db2442267111bffc1556fcf0b228f951103fe
Mintaek_CHO <mintaek5555@gmail.com>
2022-11-29 13:56:03 +0900

    fix : EC2 서버 timezone setting

--- server/src/main/java/com/codestates/mainproject/MainprojectApplication.java
+++ server/src/main/java/com/codestates/mainproject/MainprojectApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MainprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainprojectApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
