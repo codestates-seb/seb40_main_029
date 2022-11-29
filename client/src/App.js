20727130a68e51ef387ed2aa4c9fbfda24844122
kosmos992 <yechan5870@gmail.com>
2022-11-29 22:35:03 +0900

    feat: 통계 즉각 업데이트

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,50 @@ import Home from './components/templates/Home';
 // import Signup from './templates/Signup';
 import axios from 'axios';
 
+import MoodSelector from './components/module/MoodSelector';
+import LookBack from './components/module/LookBack';
+import TodoList from './components/module/TodoList';
+
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPaletteCode } from './redux/slice';
+
 axios.defaults.withCredentials = true;
 
 const App = () => {
-  const curr = window.location.toString();
-  console.log(curr);
+  const dispatch = useDispatch();
+  const [refresh, setRefresh] = useState(-1);
+  const refresher = () => {
+    setRefresh(refresh * -1);
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        'http://ec2-15-165-76-0.ap-northeast-2.compute.amazonaws.com:8080/mood/day/회원1'
+      )
+      .then(
+        // res => dispatch(setPaletteCode(res.data.moodPaletteDetails.paletteCode))
+        dispatch(setPaletteCode('p006'))
+      );
+    axios.get(
+      'http://ec2-15-165-76-0.ap-northeast-2.compute.amazonaws.com:8080/palette/'
+    );
+    // .then(res => console.log(res.data));
+  }, []);
+
   return (
     <div className="App">
+      <MoodSelector refresher={refresher} />
+      <TodoList refresher={refresher} />
+      <LookBack refresh={refresh} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/signup" element={<Signup />} /> */}
+        {/* <Route path="/mood" element={<MoodSelector />} />
+        <Route path="/look" element={<LookBack />} />
+        <Route path="/todo" element={<TodoList />} /> */}
       </Routes>
     </div>
   );
