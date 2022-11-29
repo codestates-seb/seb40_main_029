c7ecb08ac3ffca48492ec124577c23c217c59349
Ko-Eunseo <koyk0408@naver.com>
2022-11-29 21:02:45 +0900

    feat : api url 복구

--- client/src/api/FriendDataApi.js
+++ client/src/api/FriendDataApi.js
@@ -3,25 +3,25 @@ const url = `${process.env.REACT_APP_NGROK_URL}`;
 const serverUrl = `${process.env.REACT_APP_BASIC_URL}`;
 
 export const getFriends = async memberId => {
-  // const res = await axios.get(serverUrl + '/members/friend' + `/${memberId}`);
-  const res = await axios.get(url + '/members/friend' + `/${memberId}`);
+  const res = await axios.get(serverUrl + '/members/friend' + `/${memberId}`);
+  // const res = await axios.get(url + '/members/friend' + `/${memberId}`);
   return res.data;
 };
 
 export const getAllPalette = async () => {
-  const res = await axios.get(url + '/palette');
+  const res = await axios.get(serverUrl + '/palette');
   return res.data;
 };
 
 export const getSpecificPalette = async paletteCode => {
-  // const res = await axios.get(serverUrl + `/palette/${paletteCode}`);
-  const res = await axios.get(url + `/palette/${paletteCode}`);
+  const res = await axios.get(serverUrl + `/palette/${paletteCode}`);
+  // const res = await axios.get(url + `/palette/${paletteCode}`);
   return res.data;
 };
 
 export const getAllMembers = async () => {
-  // const res = await axios.get(serverUrl + '/members');
-  const res = await axios.get(url + '/members');
+  const res = await axios.get(serverUrl + '/members');
+  // const res = await axios.get(url + '/members');
   return res.data;
 };
 
@@ -29,19 +29,19 @@ export const addFriend = async ({
   requesterDisplayName,
   respondentDisplayName,
 }) => {
-  // const res = await axios.post(serverUrl + '/members/addFriend', {
-  //   requesterDisplayName,
-  //   respondentDisplayName,
-  // });
-  const res = await axios.post(url + '/members/addFriend', {
+  const res = await axios.post(serverUrl + '/members/addFriend', {
     requesterDisplayName,
     respondentDisplayName,
   });
+  // const res = await axios.post(url + '/members/addFriend', {
+  //   requesterDisplayName,
+  //   respondentDisplayName,
+  // });
   return res.data;
 };
 
 export const deleteFriend = async friendId => {
-  // const res = await axios.delete(serverUrl + `/members/friend/${friendId}`);
-  const res = await axios.delete(url + `/members/friend/${friendId}`);
+  const res = await axios.delete(serverUrl + `/members/friend/${friendId}`);
+  // const res = await axios.delete(url + `/members/friend/${friendId}`);
   return res.data;
 };
--- client/src/api/MailDataApi.js
+++ client/src/api/MailDataApi.js
@@ -3,33 +3,33 @@ const url = `${process.env.REACT_APP_NGROK_URL}`;
 const serverUrl = `${process.env.REACT_APP_BASIC_URL}`;
 
 export const getAllMails = async memberId => {
-  // const res = await axios.get(serverUrl + '/mails' + `/${memberId}`);
-  const res = await axios.get(url + '/mails' + `/${memberId}`);
+  const res = await axios.get(serverUrl + '/mails' + `/${memberId}`);
+  // const res = await axios.get(url + '/mails' + `/${memberId}`);
   return res.data;
 };
 
 export const deleteMail = async (memberId, mailId) => {
-  // const res = await axios.delete(serverUrl + `/mails/${memberId}/${mailId}`);
-  const res = await axios.delete(url + `/mails/${memberId}/${mailId}`);
+  const res = await axios.delete(serverUrl + `/mails/${memberId}/${mailId}`);
+  // const res = await axios.delete(url + `/mails/${memberId}/${mailId}`);
   return res.data;
 };
 
 export const sendMail = async ({ body, senderName, receiverName }) => {
-  // const res = await axios.post(serverUrl + '/mails', {
-  //   body,
-  //   senderName,
-  //   receiverName,
-  // });
-  const res = await axios.post(url + '/mails', {
+  const res = await axios.post(serverUrl + '/mails', {
     body,
     senderName,
     receiverName,
   });
+  // const res = await axios.post(url + '/mails', {
+  //   body,
+  //   senderName,
+  //   receiverName,
+  // });
   return res.data;
 };
 
 export const readMail = async (memberId, mailId) => {
-  // const res = await axios.patch(serverUrl + `/mails/${memberId}/${mailId}`);
-  const res = await axios.patch(url + `/mails/${memberId}/${mailId}`);
+  const res = await axios.patch(serverUrl + `/mails/${memberId}/${mailId}`);
+  // const res = await axios.patch(url + `/mails/${memberId}/${mailId}`);
   return res.data;
 };
