542608d83bafb3084a54d6659b518cd8574f744b
kosmos992 <yechan5870@gmail.com>
2022-12-01 20:55:43 +0900

    feat: try github action5

--- client/src/components/templates/Login.js
+++ client/src/components/templates/Login.js
@@ -4,5 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import GoogleLogin from '../module/GoogleLogin';
 
 export default function Login() {
+  console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
   return <GoogleLogin text="로그인" />;
 }
