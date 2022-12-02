5efec8ef6bdc2af22833c6bfac56601e7720f030
Mintaek_CHO <mintaek5555@gmail.com>
2022-12-02 16:56:00 +0900

    feat : ExceptionHandler 사용한 AOP 예외처리 구현

--- server/src/main/java/com/codestates/mainproject/exception/ExceptionController.java
+++ server/src/main/java/com/codestates/mainproject/exception/ExceptionController.java
@@ -1,17 +1,32 @@
 package com.codestates.mainproject.exception;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<?> handleBusinessLogicException(BusinessLogicException exception) {
-        return ResponseEntity.status(exception.getExceptionCode().getStatus())
-                .body(exception.getExceptionCode().getMessage());
+    public ResponseEntity<?> handleBusinessLogicException(BusinessLogicException exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+        return ResponseEntity.status(exception.getExceptionCode().getStatus()).body(response);
     }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<?> handleJwtException(JwtException exception) {
+        return ResponseEntity.status(401).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleJwtException(Exception exception) {
+        return ResponseEntity.status(200).body(exception.getMessage());
+    }
+
 }
