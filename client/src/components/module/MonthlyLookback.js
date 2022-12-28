e16680e58b41ff44f7f684a038f9ad2991cb1f52
YeryunJung <2bites@naver.com>
2022-12-28 12:14:07 +0900

    #131 - refactor: 색테마 상위색상 개수별로 색 적용

--- client/src/components/module/MonthlyLookback.js
+++ client/src/components/module/MonthlyLookback.js
@@ -91,14 +91,43 @@ const MonthlyLookback = () => {
     const loadData = async () => {
       const topColorArr = await topFourColors(displayName);
       {
-        topColorArr
-          ? setTopColors([
-              `#${topColorArr[0]}`,
-              `#${topColorArr[1]}`,
-              `#${topColorArr[2]}`,
-              `#${topColorArr[3]}`,
-            ])
-          : setTopColors(['#E7AF8D', '#F0DCB1', '#BEB5BF', '#A2A987']); // 값이 안들어왔을 경우 보여지는 임의의 색상
+        if (topColorArr.length >= 4) {
+          setTopColors([
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[1]]}`,
+            `${palettes[code][topColorArr[2]]}`,
+            `${palettes[code][topColorArr[3]]}`,
+          ]);
+          return;
+        }
+        if (topColorArr.length === 3) {
+          setTopColors([
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[1]]}`,
+            `${palettes[code][topColorArr[2]]}`,
+            `${palettes[code][topColorArr[0]]}`,
+          ]);
+          return;
+        }
+        if (topColorArr.length === 2) {
+          setTopColors([
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[1]]}`,
+            `${palettes[code][topColorArr[1]]}`,
+          ]);
+          return;
+        }
+        if (topColorArr.length === 1) {
+          setTopColors([
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[0]]}`,
+            `${palettes[code][topColorArr[0]]}`,
+          ]);
+          return;
+        }
+        setTopColors(['#E7AF8D', '#F0DCB1', '#BEB5BF', '#A2A987']); // 값이 안들어왔을 경우 보여지는 임의의 색상
       }
     };
     loadData();
