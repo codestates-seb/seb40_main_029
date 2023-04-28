1597ef6d68010e31deddf340f7f7f0d36e4420f1
Ko-Eunseo <koyk0408@naver.com>
2023-04-28 15:16:12 +0900

    refactor: 리액트쿼리 쿼리클라이언트 provider에 추가

--- client/src/index.js
+++ client/src/index.js
@@ -8,17 +8,22 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from './redux/store';
 import store from './redux/store';
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CookiesProvider>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <GlobalStyle />
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <GlobalStyle />
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   </CookiesProvider>
 );
