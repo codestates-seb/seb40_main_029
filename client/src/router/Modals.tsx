7cbb3d8964e7979e29259e61ffbfd160e7336b98
Ko-Eunseo <koyk0408@naver.com>
2023-05-02 18:47:03 +0900

    refactor: modal list 분리

---
+++ client/src/router/Modals.tsx
@@ -0,0 +1,85 @@
+import Letter from '../components/templates/modals/letter/Letter';
+import TodoList from '../components/templates/modals/todo/TodoList';
+import Friends from '../components/templates/modals/friend/Friend';
+import ThemeStore from '../components/templates/modals/theme/ThemeStore';
+import MonthlyLookback from '../components/templates/modals/monthly/MonthlyLookback';
+import LookBack from '../components/templates/modals/yearly/YearlyLookBack';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCalendarDays,
+  faEnvelope,
+  faFilm,
+  faHighlighter,
+  faStore,
+  faUserGroup,
+} from '@fortawesome/free-solid-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+interface Modal {
+  type: string;
+  component: JSX.Element;
+  label: string;
+  ModalIcon: typeof FontAwesomeIcon;
+  iconProp?: IconProp;
+  isFull: boolean;
+  withAuth: boolean;
+}
+
+export const modalList: Modal[] = [
+  {
+    type: 'LetterModal',
+    component: <Letter />,
+    label: '편지',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faEnvelope,
+    isFull: false,
+    withAuth: false,
+  },
+  {
+    type: 'TodoModal',
+    component: <TodoList lookbackRefresher={undefined} />,
+    label: '오늘할일',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faHighlighter,
+    isFull: false,
+    withAuth: false,
+  },
+  {
+    type: 'FriendModal',
+    component: <Friends />,
+    label: '친구',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faUserGroup,
+    isFull: false,
+    withAuth: false,
+  },
+  {
+    type: 'ThemeModal',
+    component: <ThemeStore />,
+    label: '색상테마',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faStore,
+    isFull: false,
+    withAuth: false,
+  },
+  {
+    type: 'MonthlyModal',
+    component: <MonthlyLookback setHiddenCard={undefined} />,
+    label: '한달기록',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faCalendarDays,
+    isFull: true,
+    withAuth: true,
+  },
+  {
+    type: 'LookbackModal',
+    component: (
+      <LookBack lookbackRefresh={undefined} setHiddenCard={undefined} />
+    ),
+    label: '일년기록',
+    ModalIcon: FontAwesomeIcon,
+    iconProp: faFilm,
+    isFull: true,
+    withAuth: true,
+  },
+];
