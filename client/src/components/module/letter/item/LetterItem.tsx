7d03f568723aaec41307cf4a4252e7607bdc939d
Ko-Eunseo <koyk0408@naver.com>
2023-05-02 14:54:05 +0900

    fix: 날짜 format 에러 dayjs 활용해 수정

--- client/src/components/module/letter/item/LetterItem.tsx
+++ client/src/components/module/letter/item/LetterItem.tsx
@@ -6,6 +6,12 @@ import { RightBottomLayout } from '../../../atoms/layout/Layouts';
 import * as Style from './Style';
 import { Mail } from '../Mail';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
 
 interface LetterPropsType {
   letter: Mail; //편지 객체
@@ -20,25 +26,13 @@ const LetterItem = ({
 }: LetterPropsType) => {
   const { mailId, senderDisplayName, createdAt, body, verifyMail } = letter;
   const memberId = useSelector(memberIdSelector);
-  let date = 0;
-  const FormatDate = (day: string) => {
-    const formatter = new Intl.RelativeTimeFormat('ko', { numeric: 'auto' });
-    const sendDay = new Date(day);
-    const today = new Date();
-    const base = Math.ceil(
-      (sendDay.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
-    );
-    date = Number(formatter.format(base > -1 ? -1 : base, 'day'));
-  };
-  FormatDate(createdAt);
 
-  let detailDate = 0;
-  const FormatDetailDate = (day: string) => {
-    const formatter = new Intl.DateTimeFormat('ko');
-    const sendDay = new Date(day);
-    detailDate = Number(formatter.format(sendDay));
+  const getRelativeTime = (date: string) => {
+    return dayjs(date).fromNow();
   };
-  FormatDetailDate(createdAt);
+
+  let date = getRelativeTime(createdAt);
+  let detailDate = dayjs(createdAt).format('YYYY년 MM월 DD일');
 
   interface LetterArg {
     memberId: number;
