78132a181a5a29e7ffc94b05f6c1ae19637cb536
Ko-Eunseo <koyk0408@naver.com>
2023-05-04 23:47:02 +0900

    refactor: getPoint 리팩토링

--- client/src/api/GetPointApi.js
+++ client/src/api/GetPointApi.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 export const getPoint = async memberId => {
   const getURL = process.env.REACT_APP_BASIC_URL;
   const path = `/members/${memberId}`;
-  return await axios.get(getURL + path).then(res => {
-    // console.log(res.data.point);
-    return res.data.point;
-  });
+  const res = await axios.get(getURL + path);
+  return res.data.point;
 };
